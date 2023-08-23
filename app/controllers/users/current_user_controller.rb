b2f7ba29eb861999ebd224cd9edec007e8693f42
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-08-23 11:07:42 -0700

    created current_user controller

--- app/controllers/users/current_user_controller.rb
+++ app/controllers/users/current_user_controller.rb
@@ -2,8 +2,7 @@ class Users::CurrentUserController < ActionController::API
   before_action :doorkeeper_authorize!
 
   def current_user
-    p 'Token'
-    p doorkeeper_token
-    return render json: { 'test': 'test'}
+    user = User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+    render json: UserSerializer.new(user).serializable_hash[:data][:attributes]
   end
 end
