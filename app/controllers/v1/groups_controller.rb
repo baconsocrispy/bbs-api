1439f459b43304ed5793ce787399908ca89c6f2b
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-09-04 16:51:26 -0700

    added serialize_group method to group controller

--- app/controllers/v1/groups_controller.rb
+++ app/controllers/v1/groups_controller.rb
@@ -1,16 +1,21 @@
 class V1::GroupsController < ApplicationController
+  before_action :doorkeeper_authorize!, except: %i[ index show ]
   before_action :set_group, only: %i[ show update destroy ]
 
   # GET /groups
   def index
     @groups = Group.all
 
-    render json: @groups
+    render json: {
+      groups: @groups.map { |group| 
+        serialize_group(group)
+      }
+    }, status: :ok
   end
 
   # GET /groups/1
   def show
-    render json: @group
+    render json: serialize_group(@group)
   end
 
   # POST /groups
@@ -18,7 +23,7 @@ class V1::GroupsController < ApplicationController
     @group = Group.new(group_params)
 
     if @group.save
-      render json: @group, status: :created
+      render json: serialize_group(@group), status: :created
     else
       render json: @group.errors, status: :unprocessable_entity
     end
@@ -27,7 +32,7 @@ class V1::GroupsController < ApplicationController
   # PATCH/PUT /groups/1
   def update
     if @group.update(group_params)
-      render json: @group
+      render json: serialize_group(@group)
     else
       render json: @group.errors, status: :unprocessable_entity
     end
@@ -46,6 +51,12 @@ class V1::GroupsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def group_params
-      params.require(:group).permit(:name, :short_description, :category_id)
+      params.require(:group)
+            .permit(:name, :short_description, :category_id, :group_image)
+    end
+
+    # serialize group
+    def serialize_group(group)
+      V1::GroupSerializer.new(group).serializable_hash[:data][:attributes]
     end
 end
