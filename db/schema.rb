02d9278a88aa5594edc295353860825cb25c6172
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-09-20 11:41:03 -0700

    added features_header to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_20_183132) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_20_183900) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -115,6 +115,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_20_183132) do
     t.datetime "updated_at", null: false
     t.bigint "group_id", null: false
     t.string "slug", null: false
+    t.string "features_header"
     t.index ["group_id"], name: "index_products_on_group_id"
   end
 
