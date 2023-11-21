e02db5b5845a281547cb60c60d738d983187ac3e
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2023-11-21 11:51:52 -0800

    updated production.rb to have host domain of api.brothers-sonsamerica.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
-  Rails.application.routes.default_url_options[:host] = 'api.carterbacon.com'
+  Rails.application.routes.default_url_options[:host] = 'api.brothers-sonsamerica.com'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
